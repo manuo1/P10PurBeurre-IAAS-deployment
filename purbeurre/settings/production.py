ccec684d215226d5b5926d054edfd7c432a58351
manuo1 <manuo1@free.fr>
2021-02-03 22:51:57 +0100

    separation of environments, work on pb on prob

--- purbeurre/settings/production.py
+++ purbeurre/settings/production.py
@@ -1,6 +1,6 @@
 from .common import *
 
-ALLOWED_HOSTS = ["165.22.118.210"]
+ALLOWED_HOSTS = ["165.22.118.210", "localhost", "127.0.0.1"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
