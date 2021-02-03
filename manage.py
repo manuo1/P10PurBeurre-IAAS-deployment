fd3d872fc5b2b0362b2df9ee8ba985ccea8cd4c4
manuo1 <manuo1@free.fr>
2021-02-03 23:32:55 +0100

    add env selection to wsgi.py

--- manage.py
+++ manage.py
@@ -6,10 +6,15 @@ from dotenv import find_dotenv, load_dotenv
 
 def main():
     """Run administrative tasks."""
+
+    """this part will designate the right settings file """
+    """to use depending on the environment"""
     load_dotenv(find_dotenv())
     environment = os.environ['ENVIRONMENT']
     os.environ.setdefault('DJANGO_SETTINGS_MODULE',
         'purbeurre.settings.{}'.format(environment))
+    """ end of environment settings part"""
+
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
--- purbeurre/wsgi.py
+++ purbeurre/wsgi.py
@@ -7,9 +7,16 @@ https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
 """
 
 import os
-
+from dotenv import find_dotenv, load_dotenv
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'purbeurre.settings')
+
+"""this part will designate the right settings file """
+"""to use depending on the environment"""
+load_dotenv(find_dotenv())
+environment = os.environ['ENVIRONMENT']
+os.environ.setdefault('DJANGO_SETTINGS_MODULE',
+    'purbeurre.settings.{}'.format(environment))
+""" end of environment settings part"""
 
 application = get_wsgi_application()
