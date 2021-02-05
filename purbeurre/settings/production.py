f2749fc4d0c411988b3cf338a8f153ed01200825
manuo1 <manuo1@free.fr>
2021-02-05 16:55:59 +0100

    change sentry lvl in production.py 4

--- purbeurre/settings/production.py
+++ purbeurre/settings/production.py
@@ -2,34 +2,53 @@ import sentry_sdk
 
 from .common import *
 from sentry_sdk.integrations.django import DjangoIntegration
-from sentry_sdk.integrations.logging import LoggingIntegration
 
 ALLOWED_HOSTS = ["165.22.118.210"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-sentry_logging = LoggingIntegration(
-    level=logging.INFO,        # Capture info and above as breadcrumbs
-    event_level=logging.INFO  # Send errors as events
-)
-
 sentry_sdk.init(
     dsn="https://751878c9886d41ed853ed05124aa3c62@o516111.ingest.sentry.io/5622155",
-    integrations=[DjangoIntegration(), sentry_logging],
-
-    # Set traces_sample_rate to 1.0 to capture 100%
-    # of transactions for performance monitoring.
-    # We recommend adjusting this value in production,
+    integrations=[DjangoIntegration()],
     traces_sample_rate=1.0,
-
-    # If you wish to associate users to errors (assuming you are using
-    # django.contrib.auth) you may enable sending PII data.
     send_default_pii=True,
-
-    # By default the SDK will try to use the SENTRY_RELEASE
-    # environment variable, or infer a git commit
-    # SHA as release, however you may want to set
-    # something more human-readable.
-    # release="myapp@1.0.0",
 )
+
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": True,
+    "root": {
+        "level": "INFO",
+        "handlers": ["sentry"],
+    },
+    "formatters": {
+        "verbose": {
+            "format": "%(levelname)s %(asctime)s %(module)s "
+            "%(process)d %(thread)d %(message)s"
+        },
+    },
+    "handlers": {
+        "sentry": {
+            "level": "INFO",
+            "tags": {"custom-tag": "x"},
+        },
+        "console": {
+            "level": "DEBUG",
+            "class": "logging.StreamHandler",
+            "formatter": "verbose",
+        },
+    },
+    "loggers": {
+        "django.db.backends": {
+            "level": "ERROR",
+            "handlers": ["console"],
+            "propagate": False,
+        },
+        "sentry.errors": {
+            "level": "DEBUG",
+            "handlers": ["console"],
+            "propagate": False,
+        },
+    },
+}
