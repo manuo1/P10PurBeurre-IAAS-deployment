44bfebf888baa7397d8b5f32def35d0a14d53ed8
manuo1 <manuo1@free.fr>
2021-02-08 14:39:46 +0100

    change LOGGING config

--- purbeurre/settings/production.py
+++ purbeurre/settings/production.py
@@ -11,59 +11,40 @@ DEBUG = False
 sentry_sdk.init(
     dsn= "https://751878c9886d41ed853ed05124aa3c62@o516111.ingest.sentry.io/5622155",
     integrations=[DjangoIntegration()],
-
-    # Set traces_sample_rate to 1.0 to capture 100%
-    # of transactions for performance monitoring.
-    # We recommend adjusting this value in production,
     traces_sample_rate=1.0,
-
-    # If you wish to associate users to errors (assuming you are using
-    # django.contrib.auth) you may enable sending PII data.
-    send_default_pii=True,
-
-    # By default the SDK will try to use the SENTRY_RELEASE
-    # environment variable, or infer a git commit
-    # SHA as release, however you may want to set
-    # something more human-readable.
-    # release="myapp@1.0.0",
 )
 
 LOGGING = {
     'version': 1,
-    'disable_existing_loggers': True,
-    'root': {
-        'level': 'INFO', # WARNING by default. Change this to capture more than warnings.
-        'handlers': ['sentry'],
-    },
+    'disable_existing_loggers': False,
     'formatters': {
         'verbose': {
-            'format': '%(levelname)s %(asctime)s %(module)s '
-                      '%(process)d %(thread)d %(message)s'
+            'format': '{levelname} {asctime} {module} {process:d} {thread:d} {message}',
+            'style': '{',
         },
     },
     'handlers': {
         'console': {
-            'level': 'DEBUG',
+            'level': 'INFO',
             'class': 'logging.StreamHandler',
             'formatter': 'verbose'
-        }
+        },
+        'log_file': {
+            'level': 'INFO',
+            'class': 'logging.FileHandler',
+            'filename': '/home/manuo1/logs/django.log',
+        },
+
     },
     'loggers': {
-        'django.db.backends': {
-            'level': 'ERROR',
-            'handlers': ['console'],
-            'propagate': False,
+        'django': {
+            'handlers': ['console', 'log_file'],
+            'level': 'INFO',
+            'propagate': True,
         },
     },
 }
 
-
 """
-    level:
-    --------------
-    DEBUG: Low level system information for debugging purposes
-    INFO: General system information
-    WARNING: Information describing a minor problem that has occurred.
-    ERROR: Information describing a major problem that has occurred.
-    CRITICAL: Information describing a critical problem that has occurred.
+levels: DEBUG < INFO < WARNING < ERROR < CRITICAL
 """
